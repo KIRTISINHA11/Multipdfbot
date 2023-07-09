02cb6f39da3c5ba418be84c0553f66f8cbe361ce
KIRTISINHA11 <83355028+KIRTISINHA11@users.noreply.github.com>
2023-07-09 20:59:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,82 @@
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question, conversation_chain):
+    response = conversation_chain({'question': user_question})
+    chat_history = response['chat_history']
+
+    for i, message in enumerate(chat_history):
+        if i % 2 == 0:
+            print(user_template.replace("{{MSG}}", message.content))
+        else:
+            print(bot_template.replace("{{MSG}}", message.content))
+
+def main():
+    load_dotenv()
+
+    pdf_docs = input("Enter the paths of your PDFs, separated by commas:")
+    pdf_paths = pdf_docs.split(",")
+    
+    # get pdf text
+    raw_text = get_pdf_text(pdf_paths)
+
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
+
+    # create vector store
+    vectorstore = get_vectorstore(text_chunks)
+
+    # create conversation chain
+    conversation_chain = get_conversation_chain(vectorstore)
+
+    user_question = input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question, conversation_chain)
+
+if __name__ == '__main__':
+    main()
